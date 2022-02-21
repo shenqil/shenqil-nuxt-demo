e33f99b2d12d3c57bcbc8fd17267b6149e735df0
shenqi.lv <shenqi.lv@31huiyi.com>
2022-02-21 17:21:36 +0800

    修复 访问次数bug

--- server-middleware/visitsNumber.ts
+++ server-middleware/visitsNumber.ts
@@ -3,6 +3,6 @@ let visitsNumber = 0
 export default function (req:any, res:any) {
   console.log('server-middleware', req.url, Date.now())
   res.setHeader('Content-Type', 'application/json')
-  res.write(JSON.stringify({ visitsNumber: visitsNumber++ }))
+  res.write(JSON.stringify({ visitsNumber: ++visitsNumber }))
   res.end()
 }
